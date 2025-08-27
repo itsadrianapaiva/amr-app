99eec505bf3e9fdcc0b24c7c178132712b014af6
Adriana Paiva <adrianapaivausa@icloud.com>
2025-08-27 12:50:48 +0000

    style:container update

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,4 +9,25 @@ export default {
     "./src/**/*.{ts,tsx}",
   ],
 
+  theme: {
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: "1rem", // Default padding
+        sm: "1.5rem", // Padding on screens `sm` and up
+        lg: "2rem", // Padding on screens `lg` and up
+      },
+      screens: {
+        sm: "640px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1280px",
+        "2xl": "1400px",
+      },
+    },
+    extend: {
+      // keep your other theme extensions here
+    },
+  },
+  plugins: [],
 } satisfies Config;
